b121f0271422b61a25378102d77b3ee13167f8c8
culturetogo <m.mueller@culture-to-go.de>
2024-04-09 18:04:02 +0200

    new config data

--- config/project-config.js
+++ config/project-config.js
@@ -3,12 +3,13 @@
 Configuration for project setup
 */
 
-export default {
-    label: '',
-    name: '',
-    url: '',
-    instanceCounter: 0,
-    adminEmail: '',
-    pwAdmin: '',
-    pwRoot: ''
+export const config = {
+    label: 'sp_dev',
+    name: 'development instance of sammlungsportal directus instance',
+    url: 'sammlungsportal-dev.bua-dns.de',
+    instanceCounter: 105,
+    adminEmail: 'm.mueller@culture-to-go.de',
+    pwAdmin: 'K+7895+54+3498+45+h+',
+    pwMariaDB: '5+mzz+87678+4+238+gLLL9',
+    pwRoot: '7++mb+87698+2834+768+gg+8'
 }
