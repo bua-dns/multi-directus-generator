5013d12b9ee4d9ec4c56b4c87c774b231ed467c9
culturetogo <m.mueller@culture-to-go.de>
2024-04-09 18:19:01 +0200

    new config structure

---
+++ config-template/project-config-sample-data.js
@@ -0,0 +1,15 @@
+
+/*
+Configuration for project setup
+sample data
+*/
+
+export default {
+    label: 'my-project', // max 10 characters, only lowercase letters and underscores
+    name: 'My Directus Instance',
+    url: 'directus.my-project.com',
+    instanceCounter: 2, // has to be unique for each instance on given server
+    adminEmail: 'admin@my-project.com',
+    pwAdmin: 'some-strong-password',
+    pwRoot: 'another-strong-password',
+}
